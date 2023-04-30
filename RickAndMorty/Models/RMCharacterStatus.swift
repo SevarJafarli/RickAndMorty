30e09d4a1db236f5a46477bdd9a847d69459b01d
SevarJafarli <sevar.jafarli.std@bhos.edu.az>
2023-04-30 22:18:57 +0400

    Add models

---
+++ RickAndMorty/Models/RMCharacterStatus.swift
@@ -0,0 +1,14 @@
+//
+//  RMCharacterStatus.swift
+//  RickAndMorty
+//
+//  Created by Sevar Jafarli on 30.04.23.
+//
+
+import Foundation
+
+enum RMCharacterStatus: String, Codable {
+    case alive = "Alive"
+    case dead = "Dead"
+    case `unknown` = "unknown"
+}
---
+++ RickAndMorty/Models/RMOrigin.swift
@@ -0,0 +1,13 @@
+//
+//  RMOrigin.swift
+//  RickAndMorty
+//
+//  Created by Sevar Jafarli on 30.04.23.
+//
+
+import Foundation
+
+struct RMOrigin: Codable {
+    let name: String
+    let url: String
+}
